e1993a9358840fc55d8ab0ac1aaacbfd82f98c6d
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-19 18:14:56 +0100

    progress on filters

--- app/helpers/vc_firms_helper.rb
+++ app/helpers/vc_firms_helper.rb
@@ -1,5 +1,5 @@
 module VcFirmsHelper
   def stage_helper(vc_firm)
-    "#{vc_firm.seed_stage.present? ? "seed_stage": nil} #{vc_firm.series_a.present? ? "series-a" : nil} #{vc_firm.series_b.present? ? "series-b" : nil} #{vc_firm.series_c.present? ? "series-c" : nil} #{vc_firm.growth_stage.present? ? "growth-stage" : nil}"
+    "#{vc_firm.seed_stage.present? ? "seed_stage": nil} #{vc_firm.series_a.present? ? "series_a" : nil} #{vc_firm.series_b.present? ? "series_b" : nil} #{vc_firm.series_c.present? ? "series_c" : nil} #{vc_firm.growth_stage.present? ? "growth_stage" : nil}"
   end
 end
