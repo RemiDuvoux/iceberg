ac0ccfa5b5a239effbdc6c2fcb9ca0691c1891af
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-19 21:02:57 +0100

    finish filters logic for stage and add for sector

--- app/helpers/vc_firms_helper.rb
+++ app/helpers/vc_firms_helper.rb
@@ -2,4 +2,12 @@ module VcFirmsHelper
   def stage_helper(vc_firm)
     "#{vc_firm.seed_stage.present? ? "seed_stage": nil} #{vc_firm.series_a.present? ? "series_a" : nil} #{vc_firm.series_b.present? ? "series_b" : nil} #{vc_firm.series_c.present? ? "series_c" : nil} #{vc_firm.growth_stage.present? ? "growth_stage" : nil}"
   end
+
+  def sector_helper(vc_firm)
+    sectors = []
+    vc_firm.startups.each do |startup|
+      sectors << startup.sector
+    end
+    return sectors.join(" ").downcase
+  end
 end
