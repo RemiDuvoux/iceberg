d78c399dd942f7f5746eea8c93efbb62c706960f
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-21 15:49:54 +0100

    ok

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,6 +1,10 @@
 class StartupsController < ApplicationController
   def show
     @startup = Startup.find(params[:id])
+  end
 
+  def index
+    @vc_firm = VcFirm.find(params[:vc_firm_id])
+    @startups = @vc_firm.startups
   end
 end
